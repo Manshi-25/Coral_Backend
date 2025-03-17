24010195dc4ba1c7a8422820993f0f4b43fabd2c
Manshi <09317711623_ml@vipstc.edu.in>
2025-03-17 22:21:04 +0530

    adding start.sh

--- app.py
+++ app.py
@@ -136,8 +136,8 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    #app.run(host="0.0.0.0", port=port)
-    serve(app, host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
+    #serve(app, host="0.0.0.0", port=port)
     #from waitress import serve  # Use production server
     #serve(app, host="0.0.0.0", port=8000)
     #app.run(debug=True)
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+export PORT=${PORT:-8000}
 waitress-serve --host=0.0.0.0 --port=$PORT app:app
